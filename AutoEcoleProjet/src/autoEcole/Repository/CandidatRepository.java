8dae73bf4587679389f9eb45c62f9865ec78deca
asus <asus@DESKTOP-ULF63H8.mshome.net>
2025-11-13 12:18:32 +0100

    ..

--- AutoEcoleProjet/src/autoEcole/Repository/CandidatRepository.java
+++ AutoEcoleProjet/src/autoEcole/Repository/CandidatRepository.java
@@ -1,5 +1,26 @@
 package autoEcole.Repository;
 
+import autoEcole.Entities.Candidat;
+
 public class CandidatRepository {
 	
+	public void save(Candidat c) {
+		
+	}
+	
+	public void findAll() {
+		
+	}
+	
+	public void findByCin(String cin) {
+		
+	}
+	
+	public void update(Candidat c) {
+		
+	}
+	
+	public void delete(String cin) {
+		
+	}
 }
