833b38cced12fba8d4e8a8532db400677cd97a06
THINPAD <THINPAD@DESKTOP-H9KUGI0>
2025-11-13 11:30:20 +0100

    Candidat

--- AutoEcoleProjet/src/autoEcole/Candidat/Candidat.java
+++ AutoEcoleProjet/src/autoEcole/Candidat/Candidat.java
@@ -1,11 +1,22 @@
 package autoEcole.Candidat;
 
 public class Candidat {
+	
 	private String nom;
 	private String prenom;
 	private String adresse;
 	private String telephone;
 	private String cin;
 	private String typePermis;
+	
+	public Candidat(String nom, String prenom, String adresse, String telephone, String cin, String typePermis) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+		this.adresse = adresse;
+		this.telephone = telephone;
+		this.cin = cin;
+		this.typePermis = typePermis;
+	}
 	
 }
