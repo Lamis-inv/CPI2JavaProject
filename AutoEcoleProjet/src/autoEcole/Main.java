46d5457d074d432f3ecea94bafdfd66e9e546b93
THINPAD <THINPAD@DESKTOP-H9KUGI0>
2025-11-13 12:13:28 +0100

    main

--- AutoEcoleProjet/src/autoEcole/Main.java
+++ AutoEcoleProjet/src/autoEcole/Main.java
@@ -1,10 +1,14 @@
 package autoEcole;
 
+import com.google.gson.Gson;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		        Gson gson = new Gson();
+		        String json = gson.toJson("Hello world!");
+		        System.out.println(json);
 
-	}
+	}	
 
 }
