7d5bac421212c0cd44edd624b8eedd83cdda0652
THINPAD <THINPAD@DESKTOP-H9KUGI0>
2025-11-13 11:28:35 +0100

    Started over

---
+++ AutoEcoleProjet/src/autoEcole/Candidat/Candidat.java
@@ -0,0 +1,11 @@
+package autoEcole.Candidat;
+
+public class Candidat {
+	private String nom;
+	private String prenom;
+	private String adresse;
+	private String telephone;
+	private String cin;
+	private String typePermis;
+	
+}
