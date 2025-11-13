3df268a3746a80468f9f5ae67d816cabcf625076
THINPAD <THINPAD@DESKTOP-H9KUGI0>
2025-11-13 12:25:51 +0100

    Candidat

--- AutoEcoleProjet/src/autoEcole/Entities/Candidat.java
+++ AutoEcoleProjet/src/autoEcole/Entities/Candidat.java
@@ -7,15 +7,19 @@ public class Candidat {
 	private String telephone;
 	private String cin;
 	private String typePermis;
+	private int nbSeanceCode;
+	private int nbSeanceConduite;
 	
-	public Candidat(String nom, String prenom, String adresse, String telephone, String cin, String typePermis) {
-		super();
+	public Candidat(String nom, String prenom, String adresse, String telephone, String cin, String typePermis, int nbSeanceCode, int nbSeanceConduite ) {
 		this.nom = nom;
 		this.prenom = prenom;
 		this.adresse = adresse;
 		this.telephone = telephone;
 		this.cin = cin;
 		this.typePermis = typePermis;
+		this.nbSeanceCode = nbSeanceCode;
+		this.nbSeanceConduite = nbSeanceConduite;
+		
 	}
 		
 }
